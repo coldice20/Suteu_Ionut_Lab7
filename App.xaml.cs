106874df8fa318d42cde421fbafdc5018d009d43
IONUȚ-EMANUEL ȘUTEU <ionut.suteu@stud.ubbcluj.ro>
2024-12-30 06:39:45 -0800

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace SuteuIonutLab7
+﻿using SuteuIonutLab7.Data;
+
+namespace SuteuIonutLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
