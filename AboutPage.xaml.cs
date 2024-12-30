aa93f2951fc33a0fcec065a9d08752424a80ce9b
IONUȚ-EMANUEL ȘUTEU <ionut.suteu@stud.ubbcluj.ro>
2024-12-30 01:03:01 -0800

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SuteuIonutLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SuteuIonutLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
