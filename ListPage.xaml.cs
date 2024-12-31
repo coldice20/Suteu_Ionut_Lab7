127c32be6a2837a4e6ededca4129e7d97ea847da
IONUȚ-EMANUEL ȘUTEU <ionut.suteu@stud.ubbcluj.ro>
2024-12-31 01:57:10 -0800

    Lab9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,25 +1,60 @@
 using SuteuIonutLab7.Models;
 
-namespace SuteuIonutLab7;
-
-public partial class ListPage : ContentPage
+namespace SuteuIonutLab7
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
-    async void OnSaveButtonClicked(object sender, EventArgs e)
+    public partial class ListPage : ContentPage
     {
-        var slist = (ShopList)BindingContext;
-        slist.Date = DateTime.UtcNow;
-        await App.Database.SaveShopListAsync(slist);
-        await Navigation.PopAsync();
-    }
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
 
-    async void OnDeleteButtonClicked(object sender, EventArgs e)
-    {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+
+        async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+        {
+            if (listView.SelectedItem is Product selectedProduct)
+            {
+                await App.Database.DeleteProductAsync(selectedProduct);
+                var shopl = (ShopList)BindingContext;
+                listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+                listView.SelectedItem = null;
+            }
+            else
+            {
+                await DisplayAlert("Error", "Please select an item to delete.", "OK");
+            }
+        }
+
     }
 }
---
+++ ValidationBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+
+namespace SuteuIonutLab7
+{
+    public class ValidationBehaviour : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+                string.IsNullOrEmpty(args.NewTextValue) ?
+                Color.FromRgba("#AA4A44") :
+                Color.FromRgba("#FFFFFF");
+        }
+    }
+}
+
